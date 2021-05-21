c8ab4ff292e05b4cd20a3dbb14f2e3323d0fb477
Agus Janardana <bjanardana@gmail.com>
2021-05-21 16:20:29 +0800

    AddedForm

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const blockchain = require('./block');
+var path = require('path');
 
 const app = express();
 const port = 3000;
@@ -11,8 +12,11 @@ app.get('/', (req, res) => {
   res.json(blockchain);
   console.log(blockchain);
 });
+app.get('/home', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
+});
 
-app.post('/', (req, res) => {
+app.post('/blockchain', (req, res) => {
   res.json(req.body);
   block = req.body;
   // console.log(typeof(block.previousHash));
